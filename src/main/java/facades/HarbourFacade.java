35505f1cda8d2c78331f8aec7f8c31a5ae2f5106
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2022-01-04 14:57:34 +0100

    getAllHarbours added

--- src/main/java/facades/HarbourFacade.java
+++ src/main/java/facades/HarbourFacade.java
@@ -1,6 +1,12 @@
 package facades;
 
+import dtos.Harbour.HarboursDTO;
+import entities.Harbour;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class HarbourFacade {
 
@@ -16,5 +22,17 @@ public class HarbourFacade {
             instance = new HarbourFacade();
         }
         return instance;
+    }
+
+    public HarboursDTO getAllHarbours() {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            TypedQuery<Harbour> query = em.createQuery("SELECT h from Harbour h", Harbour.class);
+            List<Harbour> result = query.getResultList();
+            return new HarboursDTO(result);
+        } finally {
+            em.close();
+        }
     }
 }
--- src/main/java/rest/HarbourResource.java
+++ src/main/java/rest/HarbourResource.java
@@ -1,12 +1,33 @@
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.BoatFacade;
+import facades.HarbourFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/harbour")
 public class HarbourResource {
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final HarbourFacade facade = HarbourFacade.getInstance(EMF);
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @Path("/all")
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAll() {
+        return gson.toJson(facade.getAllHarbours());
     }
 }
